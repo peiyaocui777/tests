03ead1124f1678cada46653753411017ae20ff48
202105010149XYC <norbocampasjb9657@gmail.com>
2025-02-25 19:48:05 +0800

    问题修改 加上提示

--- src/com/cpystu/houserent/utils/Utility.java
+++ src/com/cpystu/houserent/utils/Utility.java
@@ -9,13 +9,13 @@ package com.cpystu.houserent.utils;
 import java.util.*;
 /**
 
-	
+
 */
 public class Utility {
 	//静态属性。。。
     private static Scanner scanner = new Scanner(System.in);
 
-    
+
     /**
      * 功能：读取键盘输入的一个菜单选项，值：1——5的范围
      * @return 1——5
@@ -25,7 +25,7 @@ public class Utility {
         for (; ; ) {
             String str = readKeyBoard(1, false);//包含一个字符的字符串
             c = str.charAt(0);//将字符串转换成字符char类型
-            if (c != '1' && c != '2' && 
+            if (c != '1' && c != '2' &&
                 c != '3' && c != '4' && c != '5') {
                 System.out.print("选择错误，请重新输入：");
             } else break;
@@ -46,12 +46,12 @@ public class Utility {
      * @param defaultValue 指定的默认值
      * @return 默认值或输入的字符
      */
-    
+
     public static char readChar(char defaultValue) {
         String str = readKeyBoard(1, true);//要么是空字符串，要么是一个字符
         return (str.length() == 0) ? defaultValue : str.charAt(0);
     }
-	
+
     /**
      * 功能：读取键盘输入的整型，长度小于2位
      * @return 整数
@@ -81,7 +81,7 @@ public class Utility {
             if (str.equals("")) {
                 return defaultValue;
             }
-			
+
 			//异常处理...
             try {
                 n = Integer.parseInt(str);
@@ -109,8 +109,9 @@ public class Utility {
      * @param defaultValue 指定的默认值
      * @return 指定长度的字符串
      */
-	
+
     public static String readString(int limit, String defaultValue) {
+        System.out.println("请输入内容");
         String str = readKeyBoard(limit, true);
         return str.equals("")? defaultValue : str;
     }
@@ -141,28 +142,29 @@ public class Utility {
     /**
      * 功能： 读取一个字符串
      * @param limit 读取的长度
-     * @param blankReturn 如果为true ,表示 可以读空字符串。 
+     * @param blankReturn 如果为true ,表示 可以读空字符串。
      * 					  如果为false表示 不能读空字符串。
-     * 			
+     *
 	 *	如果输入为空，或者输入大于limit的长度，就会提示重新输入。
      * @return
      */
     private static String readKeyBoard(int limit, boolean blankReturn) {
-        
+
 		//定义了字符串
 		String line = "";
 
 		//scanner.hasNextLine() 判断有没有下一行
         while (scanner.hasNextLine()) {
+
             line = scanner.nextLine();//读取这一行
-           
+
 			//如果line.length=0, 即用户没有输入任何内容，直接回车
 			if (line.length() == 0) {
                 if (blankReturn) return line;//如果blankReturn=true,可以返回空串
                 else continue; //如果blankReturn=false,不接受空串，必须输入内容
             }
 
-			//如果用户输入的内容大于了 limit，就提示重写输入  
+			//如果用户输入的内容大于了 limit，就提示重写输入
 			//如果用户如的内容 >0 <= limit ,我就接受
             if (line.length() < 1 || line.length() > limit) {
                 System.out.print("输入长度（不能大于" + limit + "）错误，请重新输入：");
